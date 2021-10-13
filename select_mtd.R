0c807824253aa7059bac30dd8f79aa2d562ddcd0
staceyfu1029 <61129141+staceyfu1029@users.noreply.github.com>
2021-10-13 13:29:55 -0400

    Update select_mtd.R

--- select_mtd.R
+++ select_mtd.R
@@ -1,1 +1,11 @@
-
+iso <- function(p1,p0){
+  l <- which(p0>0)
+  p <- p1[l]/p0[l]
+  if(sum(p)==0){
+    return(max(l))
+  }
+  iso.model <- isoreg(p)
+  p.iso <- fit.isoreg(iso.model,1:length(l))
+  d <- abs(p.iso-phi)
+  l[max(which(d==min(d)))]
+}
