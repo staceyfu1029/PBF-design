435fb5dd677887802242f27529236f00f81bcc9e
staceyfu1029 <61129141+staceyfu1029@users.noreply.github.com>
2021-10-13 13:29:13 -0400

    Update get_oc.R

--- get_oc.R
+++ get_oc.R
@@ -27,7 +27,98 @@ scene <- function(phi,K){
 
 
 ## run simulations
-## Simulate trials
+## run trials including EARLY TERMINATION rules.
+trial <- function(lower,upper,skeleton,start)
+{
+  true.mtd <- which.min(abs(skeleton-phi))
+  K <- length(skeleton)
+  mtd <- rep(NA,n.sim)
+  num.p <- matrix(nrow = n.sim,ncol = K)
+  risk.over <- rep(NA,n.sim)
+  risk.under <- rep(NA,n.sim)
+  early <- rep(0,n.sim)
+  for(count in 1:n.sim){
+    if(start==1){
+      start.dose <- 1
+    }else if(start==2){
+      start.dose <- sample(c(ceiling(K/2),ceiling(K/2+0.5)),1)
+    }else{
+      start.dose <- sample(1:K,1)
+    }
+    dose.treated <- rep(0,K)
+    dose.dlt <- rep(0,K)
+    dose.next <- start.dose
+    dose.elim <- rep(1,K)
+    
+    for(i in 1:n.cohort){
+      dose.treated[dose.next] <- dose.treated[dose.next]+1
+      dlt <- rbinom(1,cohortsize,prob = skeleton[dose.next])
+      dose.dlt[dose.next] <- dose.dlt[dose.next]+dlt
+      if(dose.dlt[dose.next]<=elim.lower[dose.treated[dose.next]]){
+        dose.elim[1:dose.next] <- 0
+        if(sum(dose.elim)==0){
+          early[count] <- 1
+          mtd[count] <- dose.next
+          break
+        }
+      }
+      if(dose.dlt[dose.next]>=elim.upper[dose.treated[dose.next]]){
+        dose.elim[dose.next:K] <- 0
+        if(sum(dose.elim)==0){
+          early[count] <- 1
+          mtd[count] <- dose.next
+          break
+        }
+      }
+      if(dose.dlt[dose.next]<=lower[dose.treated[dose.next]]){
+        if(dose.next < K){
+          if(dose.elim[dose.next+1]==1){
+            dose.next <- dose.next+1
+          }
+        } 
+      }else if(dose.dlt[dose.next]>=upper[dose.treated[dose.next]]){
+        if(dose.next > 1){
+          if(dose.elim[dose.next-1]==1){
+            dose.next <- dose.next-1
+          }
+        }
+      }
+    }
+    if(is.na(mtd[count])){
+      #mtd[count] <- select.mtd(target = phi,npts = dose.treated*cohortsize,ntox = dose.dlt)$MTD
+      mtd[count] <- iso(dose.dlt,dose.treated*cohortsize)
+    }
+    if(is.na(mtd[count])){
+      next
+    }else{
+      num.p[count,] <- dose.treated*cohortsize
+      risk.over[count] <- 0
+      risk.under[count] <- 0
+      if(true.mtd==1){
+        if(sum(dose.treated[2:K])*cohortsize>risk.cutoff*n){
+          risk.over[count] <- 1
+        }
+      }else if(true.mtd==K){
+        if(sum(dose.treated[1:(K-1)])*cohortsize>risk.cutoff*n){
+          risk.under[count] <- 1
+        }
+      }else{
+        if(sum(dose.treated[1:(true.mtd-1)])*cohortsize>risk.cutoff*n){
+          risk.under[count] <- 1
+        }else if(sum(dose.treated[(true.mtd+1):K])*cohortsize>risk.cutoff*n){
+          risk.over[count] <- 1
+        }
+      }
+    }
+  }
+  return(list(num.p=num.p,mtd=mtd,early=early,
+              risk.over=risk.over,risk.under=risk.under))
+}
+
+
+
+
+## Simulate trials, without early termination rules
 trial <- function(lower,upper,skeleton)
 {
   true.mtd <- which.min(abs(skeleton-phi))
