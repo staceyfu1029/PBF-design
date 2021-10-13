13aa557a378a2375b8028d2f55803c0dd9e000cf
staceyfu1029 <61129141+staceyfu1029@users.noreply.github.com>
2021-10-13 13:23:46 -0400

    Update get_oc.R

--- get_oc.R
+++ get_oc.R
@@ -1,1 +1,80 @@
+###
 
+
+
+## Simulate the true toxicity profiles
+##   Randomly generate scenarios by the pseudouniform algorithm 
+##        (see Clertant and O'Quigley,2017)
+scene <- function(phi,K){ 
+  MTD <- sample(K,1)
+  M <- rbeta(1,max(K-MTD,0.5),1)
+  B <- phi+(1-phi)*M
+  skeleton <- rep(0,K)
+  if(MTD == 1){
+    skeleton[2:K] <- sort(runif(K-1,phi,B))
+    skeleton[1] <- runif(1,max(0,2*phi-skeleton[2]),skeleton[2])
+  }else if(MTD == K){
+    skeleton[1:(K-1)] <- sort(runif(K-1,0,phi))
+    skeleton[K] <- runif(1,skeleton[K-1],min(2*phi-skeleton[K-1],B))
+  }else{
+    skeleton[1:(MTD-1)] <- sort(runif(MTD-1,0,phi))
+    skeleton[(MTD+1):K] <- sort(runif(K-MTD,phi,B))
+    d <- min(phi-skeleton[MTD-1],skeleton[MTD+1]-phi)
+    skeleton[MTD] <- runif(1,phi-d,phi+d)
+  }
+  round(skeleton,digits = 2)
+}
+
+
+## run simulations
+## Simulate trials
+trial <- function(lower,upper,skeleton)
+{
+  true.mtd <- which.min(abs(skeleton-phi))
+  K <- length(skeleton)
+  start.dose <- 1
+  mtd <- rep(0,n.sim)
+  num.p <- rep(0,K)
+  risk.over <- rep(0,n.sim)
+  risk.under <- rep(0,n.sim)
+  for(count in 1:n.sim){
+    dose.treated <- rep(0,K)
+    dose.dlt <- rep(0,K)
+    dose.next <- start.dose
+    
+    for(i in 1:n.cohort){
+      dose.treated[dose.next] <- dose.treated[dose.next]+1
+      dlt <- rbinom(1,cohortsize,prob = skeleton[dose.next])
+      dose.dlt[dose.next] <- dose.dlt[dose.next]+dlt
+      if(dose.dlt[dose.next]<=lower[dose.treated[dose.next]]){
+        dose.next <- dose.next+1
+      }else if(dose.dlt[dose.next]>=upper[dose.treated[dose.next]]){
+        dose.next <- dose.next-1
+      }
+      if(dose.next<1){
+        dose.next <- 1
+      }else if(dose.next>length(skeleton)){
+        dose.next <- length(skeleton)
+      }
+    }
+    mtd[count] <- iso(dose.dlt,dose.treated*cohortsize)
+    num.p <- num.p+dose.treated*cohortsize
+    if(true.mtd==1){
+      if(sum(dose.treated[2:K])*cohortsize>risk.cutoff*n){
+        risk.over[count] <- 1
+      }
+    }else if(true.mtd==K){
+      if(sum(dose.treated[1:(K-1)])*cohortsize>risk.cutoff*n){
+        risk.under[count] <- 1
+      }
+    }else{
+      if(sum(dose.treated[1:(true.mtd-1)])*cohortsize>risk.cutoff*n){
+        risk.under[count] <- 1
+      }else if(sum(dose.treated[(true.mtd+1):K])*cohortsize>risk.cutoff*n){
+        risk.over[count] <- 1
+      }
+    }
+  }
+  return(list(num.p=num.p,mtd=mtd,
+              risk.over=risk.over,risk.under=risk.under))
+}
