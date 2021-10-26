083c7110bf9bda875a3ae62f130591ba791e7c11
staceyfu1029 <61129141+staceyfu1029@users.noreply.github.com>
2021-10-26 14:20:49 -0400

    Update select_mtd.R

--- select_mtd.R
+++ select_mtd.R
@@ -9,3 +9,33 @@ iso <- function(p1,p0){
   d <- abs(p.iso-phi)
   l[max(which(d==min(d)))]
 }
+
+fit.isoreg <- function(iso, x0)
+{
+  if(length(x0)==1){
+    return(iso$yf)
+  }
+  o = iso$o
+  if (is.null(o))
+    o = 1:length(x0)
+  x = unique(iso$x[o])
+  y = iso$yf
+  ind = cut(x0, breaks = x, labels = FALSE, include.lowest = TRUE)
+  min.x <- min(x)
+  max.x <- max(x)
+  adjusted.knots <- iso$iKnots[c(which(iso$yf[iso$iKnots] > 0))]
+  fits = sapply(seq(along = x0), function(i) {
+    j = ind[i]
+    
+    # Find the upper and lower parts of the step
+    upper.step.n <- min(which(adjusted.knots > j))
+    upper.step <- adjusted.knots[upper.step.n]
+    lower.step <- ifelse(upper.step.n==1, 1, adjusted.knots[upper.step.n -1] )
+    
+    # Perform a liner interpolation between the start and end of the step
+    denom <- x[upper.step] - x[lower.step]
+    denom <- ifelse(denom == 0, 1, denom)
+    val <- y[lower.step] + (y[upper.step] - y[lower.step]) * (x0[i] - x[lower.step]) / (denom)
+  })
+  fits
+}
