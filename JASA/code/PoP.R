0cdf9dff83b6ad543252370f46ac7895c0752d99
staceyfu1029 <61129141+staceyfu1029@users.noreply.github.com>
2024-06-29 10:23:27 -0700

    Update PoP.

--- JASA/code/PoP.R
+++ JASA/code/PoP.R
@@ -18,18 +18,47 @@ trial <- function(lower,upper,skeleton,start)
   
   for(count in 1:n.sim){
     toxic <- matrix(nrow = n,ncol = K)
-    for(j in 1:K){
-      toxic[,j] <- rbinom(n,1,prob = skeleton[j])
+    for(i in 1:n){
+      toxic[i,1] <- rbinom(1,1,prob = skeleton[1])
+      for(j in 2:K){
+        if(toxic[i,j-1]==1){
+          toxic[i,j] <- 1
+        }else{
+          phi1 <- skeleton[j-1]
+          phi2 <- skeleton[j]
+          toxic[i,j] <- rbinom(1,1,prob = (phi2-phi1)/(1-phi1))
+        }
+      }
     }
+    
+    # }
     # Starting dose
     start.dose <- start
     
     dose.treated <- rep(0,K)
     dose.dlt <- rep(0,K)
-    dose.next <- 1
+    dose.next <- start.dose
     dose.elim <- rep(1,K)
     
     s <- n
+    
+    # while(s>0){
+    #   dose.treated[dose.next] <- dose.treated[dose.next]+1
+    #   dlt <- toxic[s,dose.next]
+    #   dose.dlt[dose.next] <- dose.dlt[dose.next]+dlt
+    #   s <- s-1
+    # 
+    #   if(dlt==1){
+    #     if(dose.dlt[dose.next]<=lower[dose.treated[dose.next]]){ # if observed dlt <= boundary, escalate
+    #       dose.next <- min(K,dose.next+1)
+    #     }else if(dose.dlt[dose.next]>=upper[dose.treated[dose.next]]){
+    #       dose.next <- max(dose.next-1,1)
+    #     }
+    #     break
+    #   }else{
+    #     dose.next <- min(K,dose.next+1)
+    #   }
+    # }
     
     while(s>0){
       dose.treated[dose.next] <- dose.treated[dose.next]+1
@@ -45,7 +74,11 @@ trial <- function(lower,upper,skeleton,start)
         }
         break
       }else{
-        dose.next <- min(K,dose.next+1)
+        if(dose.next==K){
+          break
+        }else{
+          dose.next <- min(K,dose.next+1)
+        }
       }
     }
     
@@ -58,18 +91,18 @@ trial <- function(lower,upper,skeleton,start)
       
       ## Exclusion decision
       if(dose.dlt[dose.next]<=elim.lower[dose.treated[dose.next]]){
-        dose.elim[1:dose.next] <- 0
-        if(sum(dose.elim)==0){
+        dose.elim[1:dose.next] <- -1 ## Exclude for being subtherapeutic
+        if(sum(dose.elim==1)==0){
           early[count] <- 1
-          mtd[count] <- dose.next
+          #mtd[count] <- dose.next
           break
         }
       }
       if(dose.dlt[dose.next]>=elim.upper[dose.treated[dose.next]]){
-        dose.elim[dose.next:K] <- 0
-        if(sum(dose.elim)==0){
+        dose.elim[dose.next:K] <- 0  ## Exclude for being overly toxic
+        if(sum(dose.elim==1)==0){
           early[count] <- 1
-          mtd[count] <- dose.next
+          #mtd[count] <- dose.next
           break
         }
       }
@@ -92,8 +125,8 @@ trial <- function(lower,upper,skeleton,start)
     
     
     if(is.na(mtd[count])){
-      #mtd[count] <- select.mtd(target = phi,npts = dose.treated*cohortsize,ntox = dose.dlt)$MTD
-      mtd[count] <- iso(dose.dlt,dose.treated)
+      #mtd[count] <- select.mtd(target = phi,npts = dose.treated,ntox = dose.dlt)$MTD
+      mtd[count] <- iso.pop(dose.dlt,dose.treated)
     }
     if(is.na(mtd[count])){
       next
@@ -123,53 +156,58 @@ trial <- function(lower,upper,skeleton,start)
               risk.over=risk.over,risk.under=risk.under))
 }
 
-iso <- function(p1,p0){
+iso.pop <- function(p1,p0){
   l <- which(p0>0)
-  p <- p1[l]/p0[l]
-  if(sum(p)==0){
-    return(max(l))
+  p <- (p1[l]+0.05)/(p0[l]+0.1)
+  p.var = (p1[l] + 0.05) * (p0[l] - p1[l] + 0.05)/((p0[l] + 
+                                                      0.1)^2 * (p0[l] + 0.1 + 1))
+  p.iso <- pava(p, wt = 1/p.var)
+  p.iso = p.iso + (1:length(p.iso)) * 1e-10
+  
+  ## eliminate dose based on posterior probability
+  elimi = rep(0, K)
+  for (i in 1:length(l)) {
+    if (1 - pbeta(phi, p1[l[i]] + 1, p0[l[i]] - p1[l[i]] + 1) > 0.95) {
+      elimi[l[i]:K] = 1
+      break
+    }
   }
-  iso.model <- isoreg(p)
-  p.iso <- fit.isoreg(iso.model,1:length(l))
-  d <- abs(p.iso-phi)
-  l[max(which(d==min(d)))]
+  m <- which(elimi!=1)
+  
+  l <- l[m]
+  p.iso <- p.iso[m]
+  if(length(l)==0) {return(99)}
+  l[sort(abs(p.iso - phi), index.return = T)$ix[1]]
 }
 
-fit.isoreg <- function(iso, x0)
-{
-  if(length(x0)==1){
-    return(iso$yf)
+pava <- function(x, wt = rep(1, length(x))) {
+  n <- length(x)
+  if (n <= 1) 
+    return(x)
+  if (any(is.na(x)) || any(is.na(wt))) {
+    stop("Missing values in 'x' or 'wt' not allowed")
   }
-  o = iso$o
-  if (is.null(o))
-    o = 1:length(x0)
-  x = unique(iso$x[o])
-  y = iso$yf
-  ind = cut(x0, breaks = x, labels = FALSE, include.lowest = TRUE)
-  min.x <- min(x)
-  max.x <- max(x)
-  adjusted.knots <- iso$iKnots[c(which(iso$yf[iso$iKnots] > 0))]
-  fits = sapply(seq(along = x0), function(i) {
-    j = ind[i]
-    
-    # Find the upper and lower parts of the step
-    upper.step.n <- min(which(adjusted.knots > j))
-    upper.step <- adjusted.knots[upper.step.n]
-    lower.step <- ifelse(upper.step.n==1, 1, adjusted.knots[upper.step.n -1] )
-    
-    # Perform a liner interpolation between the start and end of the step
-    denom <- x[upper.step] - x[lower.step]
-    denom <- ifelse(denom == 0, 1, denom)
-    val <- y[lower.step] + (y[upper.step] - y[lower.step]) * (x0[i] - x[lower.step]) / (denom)
-  })
-  fits
+  lvlsets <- (1:n)
+  repeat {
+    viol <- (as.vector(diff(x)) < 0)
+    if (!(any(viol))) 
+      break
+    i <- min((1:(n - 1))[viol])
+    lvl1 <- lvlsets[i]
+    lvl2 <- lvlsets[i + 1]
+    ilvl <- (lvlsets == lvl1 | lvlsets == lvl2)
+    x[ilvl] <- sum(x[ilvl] * wt[ilvl])/sum(wt[ilvl])
+    lvlsets[ilvl] <- lvl1
+  }
+  x
 }
 
 cont <- function(x,n.level){
-  ret <- rep(0,n.level)
-  for(i in 1:n.level){
+  ret <- rep(0,n.level+1)
+  for(i in c(1:n.level)){
     ret[i] <- mean(x==i,na.rm = T)
   }
+  ret[n.level+1] <- mean(x==99,na.rm = T)
   ret
 }
 
@@ -184,20 +222,11 @@ risk.cutoff <- 0.7
 
 
 ## BOIN boundary
-library(BOIN)
 prbf01 <- function(n,y,phi){
-  p <- beta(y+2,n-y+1)/beta(1+y,n-y+1)
+  #p <- beta(y+2,n-y+1)/beta(1+y,n-y+1)
+  p <- exp(lbeta(y+2,n-y+1)-lbeta(1+y,n-y+1))
   dbinom(y,n,prob=phi)*exp(1)/dbinom(y,n,prob=p)
 }
-
-f <- function(n,x){
-  ((n+2)*x/(n*x+1))^(n*x)
-}
-
-cut5 <- function(t,phi){
-  f(t,phi)*f(t,1-phi)*(1+(n/10)/t/K)^(K*t/(n/10))
-}
-
 bound <- function(n.cohort,cohortsize,phi){
   lower <- upper <- 0
   lower.ex <- upper.ex <- 0
@@ -207,7 +236,7 @@ bound <- function(n.cohort,cohortsize,phi){
     y <- lapply(x,prbf01,n=(i*cohortsize),phi=phi)
     a <- 0
     for(j in 1:length(x)){
-      if(y[[j]]<2.5){ #e,exp(1)*(1/t*log(1+t))^(1/(2*t)),exp(1)*(1/t*log(1+t))^(1/(t))
+      if(y[[j]]<2.5){ 
         if(x[j]/(i*cohortsize)<phi){
           a[j] <- 1
         }else{
@@ -245,10 +274,10 @@ elim.upper <- pbfbound[4,]
 
 
 ## run it!
-set.seed(1212)
+set.seed(2024)
 start <- 1
 n.scene <- dim(skeleton_list)[1]
-design.mtd <- data.frame(matrix(nrow=n.scene,ncol = K))
+design.mtd <- data.frame(matrix(nrow=n.scene,ncol = K+1))
 design.allo <- data.frame(matrix(nrow=n.scene,ncol = K))
 design <- matrix(nrow=n.scene,ncol = 5)
 colnames(design) <- c("pcs","pca","risk.over","risk.under","earlyrate") 
@@ -265,4 +294,4 @@ for(i in 1:n.scene){
 end <- Sys.time()
 end-Start.time
 
-save.image("pop-15-4-1.RData")
+#save.image("PoP-15-4-1.RData")
