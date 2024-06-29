407c47e3e55e7b8ab42cfd4f07b3de058fe6415f
staceyfu1029 <61129141+staceyfu1029@users.noreply.github.com>
2024-06-29 10:45:36 -0700

    Create CRM.R

---
+++ JASA/code/CRM.R
@@ -0,0 +1,190 @@
+rm(list=ls())
+## The following code are used to run trials for random scenarios
+
+## load skeleton list
+library(readxl)
+skeleton_list <- read_excel("target15-4.xlsx", col_names = FALSE)
+
+## run trials
+trial <- function(lower,upper,skeleton,start)
+{
+  true.mtd <- which.min(abs(skeleton-phi))
+  K <- length(skeleton)
+  mtd <- rep(NA,n.sim)
+  num.p <- num.tox <- matrix(nrow = n.sim,ncol = K)
+  risk.over <- rep(NA,n.sim)
+  risk.under <- rep(NA,n.sim)
+  early <- rep(0,n.sim)
+  
+  prior <- getprior(halfwidth = 0.05, target = phi, nu=ceiling(K/2),nlevel = K, model = "logistic")
+  for(count in 1:n.sim){
+    toxic <- matrix(nrow = n,ncol = K)
+    for(i in 1:n){
+      toxic[i,1] <- rbinom(1,1,prob = skeleton[1])
+      for(j in 2:K){
+        if(toxic[i,j-1]==1){
+          toxic[i,j] <- 1
+        }else{
+          phi1 <- skeleton[j-1]
+          phi2 <- skeleton[j]
+          toxic[i,j] <- rbinom(1,1,prob = (phi2-phi1)/(1-phi1))
+        }
+      }
+    }
+    
+    # }
+    # Starting dose
+    start.dose <- start
+    
+    dose.treated <- rep(0,K)
+    dose.dlt <- rep(0,K)
+    dose.next <- start.dose
+    dose.elim <- rep(1,K)
+    
+    s <- n
+    
+    # while(s>0){
+    #   dose.treated[dose.next] <- dose.treated[dose.next]+1
+    #   dlt <- toxic[s,dose.next]
+    #   dose.dlt[dose.next] <- dose.dlt[dose.next]+dlt
+    #   s <- s-1
+    # 
+    #   if(dlt==1){
+    #     if(dose.dlt[dose.next]<=lower[dose.treated[dose.next]]){ # if observed dlt <= boundary, escalate
+    #       dose.next <- min(K,dose.next+1)
+    #     }else if(dose.dlt[dose.next]>=upper[dose.treated[dose.next]]){
+    #       dose.next <- max(dose.next-1,1)
+    #     }
+    #     break
+    #   }else{
+    #     dose.next <- min(K,dose.next+1)
+    #   }
+    # }
+    
+    level <- numeric()
+    y <- numeric()
+    
+    while(s>0){
+      dose.treated[dose.next] <- dose.treated[dose.next]+1
+      level <- c(level, dose.next)
+      dlt <- toxic[s,dose.next]
+      dose.dlt[dose.next] <- dose.dlt[dose.next]+dlt
+      y <- c(y, dlt)
+      s <- s-1
+      
+      if(dlt==1){
+        mtd.hat <- crm(prior, phi, y, level)$mtd
+        if(mtd.hat<dose.next){ # if observed dlt <= boundary, escalate
+          dose.next <- max(1,dose.next-1)
+        }else if(mtd.hat>dose.next){
+          dose.next <- min(K,dose.next+1)
+        }
+        break
+      }else{
+        if(dose.next==K){
+          break
+        }else{
+          dose.next <- min(K,dose.next+1)
+        }
+      }
+    }
+    
+    while(s>0){
+      s.tr <- min(s,cohortsize)
+      dose.treated[dose.next] <- dose.treated[dose.next]+s.tr
+      level <- c(level, rep(dose.next,s.tr))
+      dlt <- sum(toxic[s+1-(1:s.tr),dose.next])
+      y <- c(y, toxic[s+1-(1:s.tr),dose.next])
+      dose.dlt[dose.next] <- dose.dlt[dose.next]+dlt
+      s <- s-s.tr
+      
+      ## No exclusion rules for CRM, but an early exclusion rule is applied
+      if (1 - pbeta(phi, dose.dlt[1] + 1, dose.treated[1] - dose.dlt[1] + 1) > 0.95) {
+        mtd[count] <- 99
+        early[count] <- 1
+        break
+      }
+      
+      ## Transition decision
+      mtd.hat <- crm(prior, phi, y, level)$mtd
+      
+      if(mtd.hat<dose.next){ 
+        dose.next <- max(1,dose.next-1)
+      }else if(mtd.hat>dose.next){
+        dose.next <- min(K,dose.next+1)
+      }
+    }
+    
+    
+    if(is.na(mtd[count])){
+      #mtd[count] <- select.mtd(target = phi,npts = dose.treated,ntox = dose.dlt)$MTD
+      mtd[count] <- crm(prior, phi, y, level)$mtd
+    }
+    if(is.na(mtd[count])){
+      next
+    }else{
+      num.p[count,] <- dose.treated
+      num.tox[count,] <- dose.dlt
+      risk.over[count] <- 0
+      risk.under[count] <- 0
+      if(true.mtd==1){
+        if(sum(dose.treated[2:K])>risk.cutoff*n){
+          risk.over[count] <- 1
+        }
+      }else if(true.mtd==K){
+        if(sum(dose.treated[1:(K-1)])>risk.cutoff*n){
+          risk.under[count] <- 1
+        }
+      }else{
+        if(sum(dose.treated[1:(true.mtd-1)])>risk.cutoff*n){
+          risk.under[count] <- 1
+        }else if(sum(dose.treated[(true.mtd+1):K])>risk.cutoff*n){
+          risk.over[count] <- 1
+        }
+      }
+    }
+  }
+  return(list(num.p=num.p,mtd=mtd,early=early,num.tox=num.tox,
+              risk.over=risk.over,risk.under=risk.under))
+}
+
+cont <- function(x,n.level){
+  ret <- rep(0,n.level+1)
+  for(i in c(1:n.level)){
+    ret[i] <- mean(x==i,na.rm = T)
+  }
+  ret[n.level+1] <- mean(x==99,na.rm = T)
+  ret
+}
+
+## simulation setting
+phi <- 0.15
+K <- 4
+n <- 30 
+cohortsize <- 1
+n.cohort <- n/cohortsize
+n.sim <- 20000
+risk.cutoff <- 0.7
+
+
+set.seed(2024)
+start <- 1
+n.scene <- dim(skeleton_list)[1]
+design.mtd <- data.frame(matrix(nrow=n.scene,ncol = K+1))
+design.allo <- data.frame(matrix(nrow=n.scene,ncol = K))
+design <- matrix(nrow=n.scene,ncol = 5)
+colnames(design) <- c("pcs","pca","risk.over","risk.under","earlyrate") 
+Start.time <- Sys.time()
+for(i in 1:n.scene){
+  skeleton <- as.numeric(skeleton_list[i,])
+  true.mtd <- which.min(abs(skeleton-phi))
+  t <- trial(lower,upper,skeleton,start = start)
+  design[i,] <- c(mean(t$mtd==true.mtd,na.rm=TRUE),mean(t$num.p[,true.mtd],na.rm = T)/n,
+                  mean(t$risk.over,na.rm = T),mean(t$risk.under,na.rm = T),mean(t$early))
+  design.mtd[i,] <- cont(t$mtd,K)
+  design.allo[i,] <- colMeans(t$num.p,na.rm = T)/n
+}
+end <- Sys.time()
+end-Start.time
+
+#save.image("CRM-15-4-1.RData")
